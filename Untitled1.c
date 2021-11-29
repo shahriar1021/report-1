1473b29f40f2071000cbf70e40fc4f32582ada3a
sahriar1021 <strangeboy195@gmail.com>
2021-11-29 23:30:27 +0600

    task1

---
+++ Untitled1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+printf("I\n\n");
+printf("  want\n\n");
+printf("    to be a\n\n");
+printf("      good\n\n");
+printf("        programmer\n\n");
+return 0;
+}
---
+++ Untitled2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num1, num2, sum;
+    printf("Input first number: ");
+    scanf("%d",& num1);
+    printf("Input second number: ");
+    scanf("%d",& num2);
+    sum=num1+num2;
+    printf("Sum = %d",sum);
+    return 0;
+}
